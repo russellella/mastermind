90afb1acb5dcb1c0fca6b91fc5beb8803ae2c97b
russellella <116303658+russellella@users.noreply.github.com>
2023-03-04 19:19:07 -0800

    Add temporary color symbol reference

---
+++ main.rb
@@ -0,0 +1,15 @@
+
+
+
+
+### Color Mark Reference ###
+# Red Circle "\u{1F534}"
+# Blue Circle "\u{1F535}"
+# Orange Circle "\u{1F7E0}"
+# Yellow Circle "\u{1F7E1}"
+# Green Circle "\u{1F7E2}"
+# Purple Circle "\u{1F7E3}"
+
+# Black Square "\u2B1B"
+# Red Square "\u{1F7E5}"
+# White Square "\u2B1C"
