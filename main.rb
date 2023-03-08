e757f96606240c93a8c067ddcf03fa2544eaacb7
russellella <116303658+russellella@users.noreply.github.com>
2023-03-07 17:54:05 -0800

    Combine Boards into one class
    -Combined HintBoard and GuessBoard into one Board class
    -Updated Game initialize to give game instructions and create Boards
    -Updated play method to correctly check user input for correct format
    -Added clear for hint_board and guess_board at end of Play

--- main.rb
+++ main.rb
@@ -19,15 +19,11 @@ hint = Array.new
   def compare(temp_array, code_array, hint)
     temp_array.map.with_index do |v, i|
       if v == code_array[i]
-        #Add red square
         hint << "\u{1F7E5}"
-        # Deletes item from array if it matches
         temp_array.delete_at(i)
       end
     end
-    # Creates array of any that are in both arrays (including duplicates)
     white_squ = (a & b).flat_map { |n| [n]*[a.count(n), b.count(n)].min }
-    # pushes white squares for each of these to hint array
     white_squ.map do |i|
       hint_board << "\u2B1C"
     end
@@ -36,64 +32,55 @@ hint = Array.new
   def display_final
     @code.map! { |key| INDEX.fetch(key)}
   end
-  
 end
 
-# Guess Board Class
-class GuessBoard
-  attr_accessor: @guess_board
+
+class Board
+  attr_accessor :board
+
   def initialize
-    @guess_board = Array.new
+    @board = Array.new
   end
-
-  def convert_input
-    @guess_board.map! { |key| INDEX.fetch(key)}
+  def to_color
+    @board.map! { |key| INDEX.fetch(key)}
   end
-end
-
-  # Initialize: Create array - empty circles (to push user input to)
-  # Method: Compare code to secret
-    # Compare 2 arrays
-      # If includes one of colors, push a white in hint board
-      # Compare each index, and if equal, push red to hint board
-  # Method: Change board
-
-
-class HintBoard
-  attr_accessor :hint_board
-  def initialize
-    @hint_board = Array.new(4) { "\u2B1B" }
+  def display
+    puts board.join("")
   end
 end
-# Hint Board Class
-  # Methods: Create array of four black squares
 
-
-# Game Class
 class Game
+  attr_accessor :guess_board, :hint_board
   def initialize
-    # Put instructions: 1: Red, 2: Orange, 3: Yellow, 4: Green, 5: Blue, 6: Purple
-    
+    @guess_board = Board.new
+    @hint_board = Board.new
+    puts "Time to play a game of Mastermind!
+    The computer will generate a code of four random colors out of six possible, and you have twelve turns to guess it.
+    After each guess, you'll get a code back telling you how close you are: a red square for each correct color guessed in the right spot, and a white square for each correct color in an incorrect spot.
+    R: Red, O: Orange, Y: Yellow, G: Green, B: Blue, P: Purple"
   end
 
   def play
-    12.times #or when game over
-      #Display Guess Board
-      #Display Hint Board
-      input = gets.chomp
+    12.times do #or when game over
+      guess_board.display
+      hint_board.display
+      while input = gets.chomp
         if input =~ /\A[ROYGBP]*\z/ && input.length == 4
-          input.split("")  # a.replace(b) - replace a with b
-          @guess_board.replace(input.split("")) #Need to test this
+          input.split("")
+          @guess_board.replace(input.split(""))
+          break
         else
           puts "Uh oh! Please only input a combination of 4 of these colors: ROYGBP"
-          input = gets.chomp
         end
-    
-    # Run checker
+      end
+      guess_board.compare # not going to work - method is in wrong class. How to do this?
+      guess_board.clear
+      hint_board.clear
+    end
   end
 
   def end_message
-    #display correct code
+    #display final code
     if # guess code == final code
       puts "You win! You guessed the code correctly."
     else
@@ -101,7 +88,7 @@ class Game
     end
 end
     
-# Where to put this? Needs to be used by both Code and Guess Board classes
+# Where to put this? Needs to be used by both Code and Board classes
 COLOR_CODE = {
   "R" => "\u{1F534}",
   "O" => "\u{1F7E0}",
@@ -110,6 +97,8 @@ COLOR_CODE = {
   "B" => "\u{1F535}",
   "P" => "\u{1F7E3}",
 }
+
+
 
 ### Color Mark Reference ###
 # Red Circle "\u{1F534}"
