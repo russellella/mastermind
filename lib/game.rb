30d52406f121876980b31e058ac046f42212ed3b
russellella <116303658+russellella@users.noreply.github.com>
2023-03-27 18:45:29 -0700

    Pseudocode for Mastermind v2
    Basic pseudocode for 2nd version of mastermind, including:
      - Option for player to be a code maker or breaker
      - Generator for possibilities for computer guesses
      - Updated starting text for game, and followup for maker/breaker
      - play_maker and compare_maker versions of play and compare
      - Alternate win/loss messages

--- lib/game.rb
+++ lib/game.rb
@@ -14,9 +14,31 @@ class Game
     a red square for each correct color guessed in the right spot, and a
     white square for each correct color in an incorrect spot.
     R: Red, O: Orange, Y: Yellow, G: Green, B: Blue, P: Purple"
+
+    # New welcome message:
+    # "It's time to play a game of Mastermind! Do you want to be a code maker or breaker?
+      # Type 'maker' or 'breaker' :"
+
+    # user_input 
+      # if maker/breaker - set player to one of these?
+
+    # maker_instructions:
+    # "The computer will attempt to crack your code.
+    # Put in a 4-color combination of the following colors: 
+    #   R: Red, O: Orange, Y: Yellow, G: Green, B: Blue, P: Purple"
+    # user_input
+
+    # breaker_instructions:
+    #   "The computer will generate a code of four random colors out of
+    #  six possible, and you have twelve turns to guess it.
+    #   After each guess, you'll get a code back telling you how close you are:
+    #   a red square for each correct color guessed in the right spot, and a
+    #   white square for each correct color in an incorrect spot.
+    #   R: Red, O: Orange, Y: Yellow, G: Green, B: Blue, P: Purple"
+
   end
 
-  def play
+  def play #play_breaker
     12.times do
       user_input
       break if check_equal
@@ -30,6 +52,14 @@ class Game
     game_end_message
   end
 
+  # play_maker
+    # Loop: (how many times?)
+    # Computer makes random guess (from possibility array)
+    # p a.sample(4) # Doesn't work correctly - need to pick from array of arrays
+    # Display computer's guess
+    # break if guess = secret_code
+    # compare_maker
+
   def user_input
     while (input = gets.chomp)
       if input =~ /\A[ROYGBP]*\z/ && input.length == 4
@@ -42,7 +72,7 @@ class Game
     end
   end
 
-  def compare
+  def compare #compare_breaker?
     temp_array = []
     temp_array.replace(@secret_code.code)
     @guess_board.board.map.with_index do |v, i|
@@ -59,6 +89,14 @@ class Game
     end
   end
 
+  #compare_maker
+    # Compare to secret code - use altered version of compare method?
+      # If value at index = secret code, push 'H' to hint board - don't need to do this
+        #else push 'Nil'?
+      # Iterate over possibility array
+          #if value of guess[i] = secret_code[i], keep it (do nothing)
+        # Else delete from array
+
   def check_equal
     if @secret_code.code == @guess_board.board
       @win = true
@@ -72,6 +110,9 @@ class Game
     else
       puts 'You lose. You did not guess the code.'
     end
+    # Alternatives to code in:
+      # "You lost - the computer guessed your code."
+      # "You won - the computer didn't guess your code."
   end
 
 end
--- main.rb
+++ main.rb
@@ -6,3 +6,11 @@ require_relative 'lib/code'
 
 new_game = Game.new
 new_game.play
+
+
+# Pseudocode for Mastermind V2
+
+# Create an array of arrays of all possibilities
+# Where do I stick this?
+a = ['R', 'O', 'Y', 'G', 'B', 'P']
+p a.repeated_permutation(4).to_a
